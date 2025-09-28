6a0e69837ccc79968540b96d238e085e5e7dab84
confused-Techie <dev@lhbasics.com>
2025-09-27 20:22:36 -0700

    Finished point add logic on site

--- static/site.js
+++ static/site.js
@@ -53,14 +53,86 @@ async function modifyPoints(modificationType, formName) {
 
   // API req time
   if (isBulk) {
+    const params = new URLSearchParams();
+    params.append("action", modificationType);
+    params.append("points", points);
+    params.append("reason", reason);
 
+    const totalReqs = studentId.length;
+    let successfulReqs = 0;
+    let failedReqs = 0;
+
+    for (let i = 0; i < studentId.length; i++) {
+      try {
+        const res = await fetch(`/api/students/${studentId[i]}/points?${params.toString()}`, { method: "POST" });
+        if (res.status === 200) {
+          successfulReqs = successfulReqs + 1;
+        } else {
+          failedReqs = failedReqs + 1;
+        }
+      } catch(err) {
+        failedReqs = failedReqs + 1;
+      }
+    }
+
+    if (successfulReqs === totalReqs) {
+      const notifParams = new URLSearchParams();
+      notifParams.append("status", "success");
+      notifParams.append("msg", "Successfully modified batch of students points.");
+      const notifRes = await fetch(`/api/notifications?${notifParams.toString()}`, { method: "PATCH" });
+      if (notifRes.status !== 200) {
+        console.error("Error adding successful batch point modification msg to notifications");
+        console.error(notifRes);
+      }
+    } else {
+      const notifParams = new URLSearchParams();
+      notifParams.append("status", "danger");
+      notifParams.append("msg", `Errors occurred while adding batch of student points. Successful '${successfulReqs}', Failed '${failedReqs}'`);
+      const notifRes = await fetch(`/api/notifications?${notifParams.toString()}`, { method: "PATCH" });
+      if (notifRes.status !== 200) {
+        console.error("Error adding failed batch point modification msg to notifications");
+        console.error(notifRes);
+      }
+    }
   } else {
     const params = new URLSearchParams();
     params.append("action", modificationType);
     params.append("points", points);
     params.append("reason", reason);
 
-    const res = await fetch(`/api/students/${studentId}/points?${params.toString()}`, { method: "POST" });
-    
+    try {
+      const res = await fetch(`/api/students/${studentId}/points?${params.toString()}`, { method: "POST" });
+      if (res.status === 200) {
+        // Everything succeeded, and we can add a notification indicating so
+        const notifParams = new URLSearchParams();
+        notifParams.append("status", "success");
+        notifParams.append("msg", `Successfully modified points for student: '${studentId}'`);
+        const notifRes = await fetch(`/api/notifications?${notifParams.toString()}`, { method: "PATCH" });
+        if (notifRes.status !== 200) {
+          console.error("Error adding successful point modification msg to notifications");
+          console.error(notifRes);
+        }
+      } else {
+        // The process failed, add a notification
+        const notifParams = new URLSearchParams();
+        notifParams.append("status", "danger");
+        notifParams.append("msg", `Failed to modify points for student: '${studentId}'`);
+        const notifRes = await fetch(`/api/notifications?${notifParams.toString()}`, { method: "PATCH" });
+        if (notifRes.status !== 200) {
+          console.error("Error adding failed point modification msg to notifications");
+          console.error(notifRes);
+        }
+      }
+    } catch(err) {
+      const notifParams = new URLSearchParams();
+      notifParams.append("status", "danger");
+      notifParams.append("msg", `Failed to modify points for student: '${studentId}' due to '${err.toString()}'`);
+      const notifRes = await fetch(`/api/notifications?${notifParams.toString()}`, { method: "PATCH" });
+      if (notifRes.status !== 200) {
+        console.error("Error adding errored point modification msg to notifications");
+        console.error(notifRes);
+      }
+    }
+
   }
 }
